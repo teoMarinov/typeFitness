36506975fa641a53e1fd4c4f951939a755c85109
Teodor Marinov <teodor.b.marinov@gmail.com>
2023-07-01 15:08:56 +0300

    Change MakeNewWorkout

--- typeFitness/src/conponents/Workouts/MakeNewWorkout.tsx
+++ typeFitness/src/conponents/Workouts/MakeNewWorkout.tsx
@@ -1,15 +1,56 @@
-import { AbsoluteCenter, Box, Center, Image, Button } from "@chakra-ui/react";
-import bgImage from "../../images/pexels-victor-freitas-841130.jpg"
+import {
+    Box,
+    SimpleGrid,
+    Stat,
+    StatLabel,
+    StatNumber,
+    useColorModeValue,
+    Center, Button
+} from "@chakra-ui/react";
+import { useState } from "react";
 
 export default function MakeNewWorkout() {
+
+    const [backgroundHeight, setbackgroundHeight] = useState("45vh")
+    const [toggleWorkoutMaker, setToggleWorkoutMaker] = useState(false)
+
+    const toggleNewWorkout = () => {
+        if (backgroundHeight === "45vh") setbackgroundHeight('65vh')
+        else setbackgroundHeight('45vh')
+        setToggleWorkoutMaker(!toggleWorkoutMaker)
+    }
+
     return (
-        <Center
+        <Stat
+            px={{ base: 4, md: 8 }}
+            py={'5'}
             width="100%"
-            height="100%"
+            height={backgroundHeight}
             bg={'blue.300'}
+            shadow={'xl'}
+            borderColor={useColorModeValue('gray.800', 'gray.500')}
         >
-            <Button bg={'blue.400'} colorScheme={"blue"}> Make New Workout</Button>
-        </Center>
+            <Center
+                height={backgroundHeight}
+            >
+                {toggleWorkoutMaker ?
+                    (< Box width={"30%"} height={'500px'} bg={'white'}>
+                        <Button bg={'red.400'} colorScheme={"blue"} onClick={toggleNewWorkout} ml={'92%'}>
+                            X
+                        </Button>
+                    </Box>)
+                    :
+                    (<Button bg={'blue.400'} colorScheme={"blue"} onClick={toggleNewWorkout}>
+                        Make New Workout
+                    </Button>
+                    )
+                }
+            </Center >
+        </Stat>
     )
 
 }
+
+
+
+
