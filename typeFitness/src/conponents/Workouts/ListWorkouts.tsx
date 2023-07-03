0fed4449c8d5dc6844004bfc9fadcd387e42b3cc
Teodor Marinov <teodor.b.marinov@gmail.com>
2023-07-03 13:30:44 +0300

    change ListWorkouts

--- typeFitness/src/conponents/Workouts/ListWorkouts.tsx
+++ typeFitness/src/conponents/Workouts/ListWorkouts.tsx
@@ -1,8 +1,8 @@
-import { Heading, Center, Text, VStack, IconButton, Flex, Box, Input } from "@chakra-ui/react"
-import { EditIcon, DeleteIcon, CloseIcon, CheckIcon } from "@chakra-ui/icons"
+import { Heading, Center, Text, VStack, Box } from "@chakra-ui/react"
 import { Workout } from "./Workouts"
 import { useState } from "react"
-import editData from "../../utils/editData"
+import EditModal from "./EditModal"
+import DeleteModal from "./DeleteModal"
 
 
 type WorkoutType = {
@@ -12,32 +12,20 @@ type WorkoutType = {
 
 export type TypeExercise = {
   name: string;
-  sets: string;
   reps: string;
+  sets: string;
+}
+
+type TypeWorkout = {
+    exercises: TypeExercise[];
+    name: string
 }
 export default function ListWorkouts({ workout, user }: WorkoutType) {
 
+
+  const workoutData : TypeWorkout = workout[1]
+  console.log(workoutData)
   const [toggleDetails, setToggleDetails] = useState(true)
-  const [toggleEditMenu, setToggleEditMenu] = useState(false)
-  const [toggleDelete, setToggleDelete] = useState(false)
-  const [editedVals, setEditedVals] = useState(workout[1].exercises)
-
-  const handleChange = (index: number, key: string, newVal: string) => {
-    const updatedExercise = { ...editedVals[index], [key]: newVal };
-    const updatedVals = [...editedVals];
-    updatedVals[index] = updatedExercise;
-    setEditedVals(updatedVals);
-  };
-
-
-  const handleAcceptEdit = () => {
-    setToggleEditMenu(false)
-    const changedValues = [...editedVals]
-  }
-
-  const handleAcceptDelete = (postId: string) => {
-    editData(`workouts/${user}`, postId, null)
-  }
 
 
   return (
@@ -51,98 +39,24 @@ export default function ListWorkouts({ workout, user }: WorkoutType) {
       _hover={{ cursor: "pointer" }}
     >
       {toggleDetails ? (<VStack position={'relative'} w={'full'} h={'100%'} textAlign={'center'} justifyContent={'center'}>
-        {toggleEditMenu ?
-          (<>
-            <IconButton
-              bg={'none'}
-              aria-label='Call Segun'
-              size='xs'
-              icon={<CloseIcon />}
-              position={'absolute'}
-              top={1}
-              right={1}
-              onClick={() => {
-                setToggleEditMenu(false)
-              }}
-            />
-            <IconButton
-              bg={'none'}
-              aria-label='Call Segun'
-              size='xs'
-              icon={<CheckIcon />}
-              position={'absolute'}
-              top={1}
-              right={7}
-              // onClick={handleAcceptEdit}
-            />
-            {toggleDelete ? (
-              <>
-                <IconButton
-                  bg={'none'}
-                  aria-label='Call Segun'
-                  size='xs'
-                  icon={<CloseIcon />}
-                  position={'absolute'}
-                  top={7}
-                  right={1}
-                  onClick={() => setToggleDelete(false)}
-                />
-                <IconButton
-                  bg={'none'}
-                  aria-label='Call Segun'
-                  size='xs'
-                  icon={<CheckIcon />}
-                  position={'absolute'}
-                  top={7}
-                  right={7}
-                  onClick={() => handleAcceptDelete(workout[0])}
-                />
-              </>
-            )
-              :
-              (<IconButton
-                bg={'none'}
-                aria-label='Call Segun'
-                size='xs'
-                icon={<DeleteIcon />}
-                position={'absolute'}
-                top={7}
-                right={1}
-                onClick={() => setToggleDelete(true)}
-              />)}
-          </>)
-          : (<IconButton
-            bg={'none'}
-            aria-label='Call Segun'
-            size='xs'
-            icon={<EditIcon />}
-            position={'absolute'}
-            top={1}
-            right={1}
-            onClick={() => setToggleEditMenu(true)}
-          />)}
-        {workout[1].exercises.map((exercise: TypeExercise, index: number) => (
-          <Box key={exercise.name + index}>
-            {
+        <Box position={'absolute'} top={1} right={1}>
+          <EditModal workout={workoutData.exercises} name={workoutData.name} id={workout[0]} currentUser={user}/>
+        </Box>
+        <Box position={'absolute'} top={10} right={1}>
+          <DeleteModal workoutId={workout[0]} workoutName={workoutData.name} currentUser={user} />
+        </Box>
+        {
+          (workoutData.exercises.map((exercise: TypeExercise, index: number) => (
+            <Box key={exercise.name + index}>
+              <Text >{exercise.name} - {exercise.sets} x {exercise.reps}</Text>
+            </Box>
+          )))}
+      </VStack>) : (
+        <>
 
-              toggleEditMenu ?
-                (<>
-                  <Flex>
-                    <Input value={editedVals[index].name} w={'350px'} onChange={(e) => handleChange(index, 'name', e.target.value)} mr={1}></Input>
-                    <Input value={editedVals[index].sets} w={'70px'} onChange={(e) => handleChange(index, 'sets', e.target.value)} mr={1}></Input>
-                    <Input value={editedVals[index].reps} w={'70px'} onChange={(e) => handleChange(index, 'reps', e.target.value)}></Input>
-                  </Flex>
-
-                </>)
-                :
-                (
-                  <Text >{exercise.name} - {exercise.sets} x {exercise.reps}</Text>
-                )}
-          </Box>
-        ))}
-      </VStack>) : (<Heading as="h2" display="block">
-        {workout[1].name}
-      </Heading>)
+          <Heading as="h2" display="block">{workoutData.name}</Heading>
+        </>
+      )
       }
     </Center >
   );
