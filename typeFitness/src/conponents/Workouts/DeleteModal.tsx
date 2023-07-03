905fcda7ad5dec914f3e269512fe0c48425a7b0e
Teodor Marinov <teodor.b.marinov@gmail.com>
2023-07-03 13:31:06 +0300

    add DeleteModal for workouts

---
+++ typeFitness/src/conponents/Workouts/DeleteModal.tsx
@@ -0,0 +1,47 @@
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    Button,
+    useDisclosure,
+    IconButton,
+} from '@chakra-ui/react'
+import { DeleteIcon } from "@chakra-ui/icons"
+import editData from "../../utils/editData"
+
+type PropType = {
+    workoutId: string;
+    workoutName: string;
+    currentUser: string
+}
+
+export default function DeleteModal({ workoutId, workoutName, currentUser }: PropType) {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+
+    const handleAcceptDelete = () => {
+        editData(`workouts/${currentUser}/${workoutId}`, null)
+        onClose()
+    }
+
+    return (
+        <>
+            <IconButton size={'sm'} aria-label='Delete' onClick={onOpen} icon={<DeleteIcon />} />
+
+                <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
+                    <ModalOverlay />
+                    <ModalContent>
+                        <ModalHeader>Are you sure you want to delete {workoutName} </ModalHeader>
+                        <ModalFooter>
+                            <Button colorScheme='blue' mr={3} onClick={handleAcceptDelete}>
+                                Delete
+                            </Button>
+                            <Button onClick={onClose}>Cancel</Button>
+                        </ModalFooter>
+                    </ModalContent>
+                </Modal>
+        </>
+    )
+}
+
