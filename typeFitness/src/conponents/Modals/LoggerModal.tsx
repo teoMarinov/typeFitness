e7aa5b8f176e4141ad6e300e7b5af0ef4bbd7cf4
Teodor Marinov <teodor.b.marinov@gmail.com>
2023-07-09 00:56:48 +0300

    add exercise log option

--- typeFitness/src/conponents/Modals/LoggerModal.tsx
+++ typeFitness/src/conponents/Modals/LoggerModal.tsx
@@ -1,6 +1,147 @@
+import { TypeExercise } from "../Workouts/ListWorkouts"
+import {
+  Box,
+  Button,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalOverlay,
+  useDisclosure,
+  Heading,
+  ModalCloseButton,
+  ModalFooter
+} from "@chakra-ui/react"
+import { useState } from "react"
+import ExerciseLogger from "../Workouts/ExerciseLogger"
+import { logType } from "../Workouts/ExerciseLogger"
+import addData from "../../utils/addData"
 
-export default function LoggerModal() {
+
+type propTypes = {
+  workout: TypeExercise[] | string
+  currentUser: string
+  unfocus: any
+}
+
+
+
+type exerciseType = logType[]
+
+type workoutDataType = {
+  name: string
+  date: string
+  exercises: exerciseType[]
+}
+
+export default function LoggerModal({ workout, currentUser, unfocus }: propTypes) {
+
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [loggedData, setLoggedData] = useState([])
+  const [currentlyOpen, setCurrentlyOpen] = useState('')
+
+
+  const handleClose = () => {
+    setCurrentlyOpen('')
+    unfocus(false)
+    onClose()
+  }
+
+  const handleFinishWorkout = () => {
+    const workoutData: workoutDataType = {
+      name: workout.name,
+      date: new Date().toString(),
+      exercises: loggedData
+    }
+    
+    console.log(loggedData)
+    console.log(workoutData)
+    try {
+      addData(`finishedWorkouts/${currentUser}`, workoutData)
+    } catch(error) {
+      console.log(error)
+    }
+
+    handleClose()
+  }
+
+
   return (
-    <div>LoggerModal</div>
+    workout.exercises ?
+      (<Box
+        w={'full'}
+        h={'100%'}
+        onClick={onOpen}
+        position={'absolute'}
+      >
+        <Modal 
+        isCentered 
+        size='460px' 
+        isOpen={isOpen} 
+        onClose={onClose} 
+        closeOnOverlayClick={false} 
+        >
+          <ModalOverlay />
+          <ModalContent
+            minH='52vh'
+            maxH='80vh'
+            width={'460px'}
+            textColor={'white'}
+            bg="rgba(0, 0, 0, 0.9)"
+            position={'relative'}
+            overflow={'auto'}
+          >
+            <ModalCloseButton
+              mt={2}
+              zIndex={1}
+              onClick={handleClose}
+            />
+            <ModalBody
+              pb={6}
+            >
+              <Heading
+                mt={2}
+                w={'full'}
+                textAlign={'center'}
+                pb={6}
+              >
+                {workout.name}
+              </Heading>
+
+              {
+                workout.exercises.map((exercise: TypeExercise, index: number) => (
+                  <Box key={index}>
+                  <ExerciseLogger
+                    exercise={exercise}
+                    loggedData={loggedData}
+                    setLoggedData={setLoggedData}
+                    currentlyOpen={currentlyOpen}
+                    setCurrentlyOpen={setCurrentlyOpen}
+                    exerciseIndex={index}
+                    />
+                    </Box>
+                ))
+              }
+
+            </ModalBody>
+            <ModalFooter>
+              <Button
+                w={'full'}
+                h={'60px'}
+                bg={'rgb(5,5,5)'}
+                _hover={{ bg: 'rgb(15,15,15)' }}
+                textColor={'white'}
+                onClick={handleFinishWorkout}
+                // onClick={() => console.log(loggedData)}
+                pos={'absolute'}
+                rounded={0}
+                bottom={0}
+                right={0}
+              >
+                Finish
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </Box>) : <></>
   )
 }
---
+++ typeFitness/src/conponents/Workouts/ExerciseLogger.tsx
@@ -0,0 +1,141 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-inferrable-types */
+import { TypeExercise } from "./ListWorkouts"
+import {
+    Text,
+    Input,
+    HStack,
+    VStack,
+    IconButton
+} from "@chakra-ui/react"
+import { useState } from 'react'
+import { NotAllowedIcon } from '@chakra-ui/icons'
+
+
+
+type propTypes = {
+    exercise: TypeExercise
+    loggedData: any
+    setLoggedData: any
+    currentlyOpen: string
+    setCurrentlyOpen: any
+    exerciseIndex: number
+}
+
+export type logType = {
+    weight: string
+    reps: string
+    failure: boolean
+}
+
+
+export default function ExerciseLogger({ exercise, loggedData, setLoggedData, currentlyOpen, setCurrentlyOpen, exerciseIndex }: propTypes) {
+    const [exerciseLoggs, setExerciseLoggs] = useState([{}])
+    const lineH = exercise.name.length > 35 ? 64 : 40
+    const lineIncrease = 38
+
+    const updateLoggedData = (newLog: logType[]) => {
+        const changedLog = [...loggedData]
+        changedLog[exerciseIndex] = newLog
+        setLoggedData(changedLog)
+    }
+
+    const handleInput = (key: string, value: string, index: string) => {
+        const lastIndex = exerciseLoggs.length - 1;
+        const updatedLoggs = [...exerciseLoggs];
+
+        updatedLoggs[index] = {
+            ...updatedLoggs[index],
+            [key]: value,
+            failure: false
+        };
+
+        setExerciseLoggs(updatedLoggs);
+
+        updateLoggedData(updatedLoggs)
+
+        if (lastIndex >= 0 && updatedLoggs[lastIndex].reps && updatedLoggs[lastIndex].weight) {
+            updatedLoggs.push({});
+        }
+    }
+
+
+
+    const toggleCurrentlyOpen = () => currentlyOpen === exercise.name ? setCurrentlyOpen('') : setCurrentlyOpen(exercise.name)
+
+    const handleToggleFailure = (index: any) => {
+        const updatedStatus = [...exerciseLoggs]
+        updatedStatus[index] = {
+            ...updatedStatus[index],
+            failure: !updatedStatus[index].failure
+        }
+        setExerciseLoggs(updatedStatus)
+        setLoggedData(updatedStatus)
+    }
+
+
+    return (
+        <>
+            <VStack
+                overflow="hidden"
+                transition="height 0.2s ease"
+                height={currentlyOpen === exercise.name ? `${exerciseLoggs.length * lineIncrease + lineH}px` : `${lineH}px`}
+                _hover={{
+                    cursor: "pointer"
+                }}
+            >
+                <Text
+                
+                    _hover={{ bg: 'gray.600' }}
+                    onClick={toggleCurrentlyOpen}
+                    rounded={'md'}
+                    textAlign={'center'}
+                    p={2}
+                    userSelect="none"
+                >
+
+                    {exercise.name} - {exercise.sets} x {exercise.reps}
+                </Text>
+                    {exerciseLoggs.map((data: any, index: any) => (
+                        <HStack key={index}>
+                            <Input
+                                p={0}
+                                w={'65px'}
+                                h={'30px'}
+                                fontSize='18'
+                                placeholder="reps"
+                                value={data.reps}
+                                textAlign={'center'}
+                                onChange={(e) => {
+                                    handleInput('reps', e.target.value, index)
+                                }}
+                            />
+                            <Input
+                                p={0}
+                                w={'65px'}
+                                h={'30px'}
+                                fontSize='18'
+                                placeholder="weight"
+                                value={data.weight}
+                                textAlign={'center'}
+                                onChange={(e) => {
+                                    handleInput('weight', e.target.value, index)
+                                }}
+                            />
+                            <IconButton
+                                size={'xs'}
+                                aria-label='Edit'
+                                textColor={'white'}
+                                border={'1px'}
+                                _hover={{ bg: data.failure ? 'rgb(250, 0, 0)' : 'rgb(30, 30, 30)' }}
+                                icon={<NotAllowedIcon />}
+                                bg={data.failure ? 'rgb(250, 0, 0)' : 'none'}
+                                onClick={() => handleToggleFailure(index)}
+                            />
+                        </HStack>
+                    ))}
+
+            </VStack>
+        </>
+    )
+}
